097dbdf71864291fae43f4fd1b92b38d803be60d
Matthew Brandenburg <matt.bran87@gmail.com>
2019-07-07 21:42:26 -0700

    add landing section

--- js/scripts.js
+++ js/scripts.js
@@ -38,10 +38,20 @@ $( document ).ready(function() {
         // $(linkHash).show(); // .show() was causing issues with toggle // TODO: fix someday maybe. not a big deal, no performace issues noticed in mobile and desktop views.
         $(linkHash).css("display", "block");
       }
-      window.location.hash = linkHash;
-      // window.location.href = window.location.href + linkHash;
-      console.log(window.location.hash);
-      console.log(window.location.href);
+      var hashData = {
+        "hash": linkHash,
+        "time": new Date()
+      };
+      localStorage.setItem('hash', JSON.stringify(hashData));
+
+      if (JSON.parse(localStorage.getItem('hash')).hash == "#landing") {
+        $('body').css({"background": "url(img/portland.jpg) no-repeat",
+          "background-position": "center",
+          "background-size": "cover"
+        });
+      } else {
+        $('body').css("background", "rgba(240,240,240,.3)");
+      }
     });
 
     // print button on resume
@@ -49,18 +59,43 @@ $( document ).ready(function() {
 		    window.print();
     })
 
+    if (JSON.parse(localStorage.getItem('hash')).hash == "#landing") {
+      $('body').css({"background": "url(img/portland.jpg) no-repeat",
+        "background-position": "center",
+        "background-size": "cover"
+      });
+    } else {
+      $('body').css("background", "rgba(240,240,240,.3)");
+    }
+
     showDefault();
+    // window.onbeforeunload = updateHistory(localStorage.getItem('hash'))
 });
 
-//
+// TODO: Fix page load. Hash not loaded on new user. Should clear cash if storage is old.
 function showDefault() {
   var visElm = $('.body-info:visible');
   if (visElm.length == 0) {
-    var hash = window.location.hash;
-    var cleanHash = hash.replace('#','');
-    if (hash.length > 0) {
+    var hashData = JSON.parse(localStorage.getItem('hash'));
+    if (hashData != undefined && hashData.hash.length > 0) {
+      var date = new Date();
+      var hashDataDate = Date(hashData.time);
+
+      console.log(date.getTime());
+      console.log(hashDataDate);
+      if (date == "") {
+
+      } else {
+        var hash = hashData.hash;
+      }
+    } else if(window.location.hash.length > 0) {
+      var hash = window.location.hash;
+    }
+    if (hash != undefined && hash.length > 0) {
+      var cleanHash = hash.replace('#','');
       $(hash).show();
       $('.menu-' + cleanHash).addClass('highlighted');
+      // localStorage.removeItem('hash')
     } else {
       $('#about').show();
       $('.menu-about').addClass('highlighted');
@@ -70,4 +105,8 @@ function showDefault() {
 
 function scrollToTop() {
   document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
+
+function updateHistory(curr) {
+    window.location.hash = curr;
 }
